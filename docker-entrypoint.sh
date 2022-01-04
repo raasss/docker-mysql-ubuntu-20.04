b24f5bb169f4d4932795ab31989dae497ba0cb2b
Radisa Jovicevic <radisa@gmail.com>
2022-01-04 21:31:55 +0100

    properly handle subprocess exit codes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,11 @@
 
 set -xe
 
-# TODO: implement supervisord for running 3 tails and mariadb. Also handle missing files.
 tail -F /var/log/mysql/query.log >/dev/stdout &
 tail -F  /var/log/mysql/error.log >/dev/stderr &
 tail -F  /var/log/mysql/mysql-slow.log >/dev/stdout &
+
+# customize mysqld conf file in runtime from environment variables
 
 for ENVVAR in $(env | grep -E '^MYSQL_SERVER_CONF_.+')
 do
@@ -15,32 +16,50 @@ do
   crudini --verbose --set "/etc/mysql/mysql.conf.d/mysqld.cnf" "${ENVVAR_SECTION}" "${ENVVAR_KEY}" "${ENVVAR_VALUE}"
 done
 
-mysqld_safe &
-MYSQLD_SAFE_PID=$!
+# print mysqld startup defaults for easy check by user of this docker image
 
-echo -n "Waiting 1min for mysql to start"
+mysqld --print-defaults
+
+# start mysqld
+
+mysqld_safe &
+
+# wait 60 seconds for mysqld to become operational
+
 for I in {0..59}; do
     if mysql -v --user=root --password="" -e "SELECT 1"; then
         break
     fi
-    echo -n .
     sleep 1s
 done
+
+# exit with error code 1 if mysqld didn't became operational after 60 seconds
 
 if [ "${I}" == "59" ]; then
     exit 1
 fi
+
+# configure database, user and access from environment variables
 
 mysql -v -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
 mysql -v -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'"
 mysql -v -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@'%'"
 mysql -v -e "FLUSH PRIVILEGES"
 
-MYSQLD_PID="$(cat /var/run/mysqld/mysqld.pid)"
-kill -s TERM "${MYSQLD_PID}"
+# wait for any subprocess to faile and get it's exit code
 
-wait "${MYSQLD_SAFE_PID}"
+set +e
+wait -n
+set -e
 
-mysqld --print-defaults
+ERROR_CODE=$?
 
-mysqld_safe
+# gracefully shutdown mysql if it is working
+
+if mysql -v -e "SELECT 1"; then
+    mysqladmin shutdown || true
+fi
+
+# exit bash with fail subprocess exit code
+
+exit ${ERROR_CODE}
